c00ad320d41b233f408a402974b19bf5c94425c5
bumrei <bumrei@gmail.com>
2021-07-09 13:24:09 +0900

    y

---
+++ hello - 복사본.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+    int main()
+    {
+        int i = -1;
+        if (i > 0) {
+            printf("Hello, world!");
+        } else {
+            printf("HaHa");
+        }
+        return 0; 
+    }
+
+// 메인이라 불리우는 블록 안에 입력
+
+//블록 1
